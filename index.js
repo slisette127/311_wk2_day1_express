2fd3cc5cdd357c6c4da6bec71709f3412190e49d
SLISETTE127 <SLISETTE127@GMAIL.COM>
2021-07-11 14:41:12 -0500

    post stuff

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ const port = process.env.PORT || 4000
 const { users } = require('./state')
 
 /* BEGIN - create routes here */
+app.get('/users', (req,res) => {
+  res.json(users)
+}
+) 
+app.get('/users/:id', (req, res) => {
+  const found = users.some(user => user._id == req.params.id)
+  if (found) {
+    res.send(users.filter(user => user._id == req.params.id))
+  }
+})
+app.post('/users', (req, res) => {
+  const newUser = {
+    "_id": 6,
+    "name": "Frank Zappa",
+    "occupation": "musician",
+    "avatar": "https://upload.wikimedia.org/wikipedia/commons/0/02/Frank_Zappa_1973_2.JPG"
+    }
+    if (!newUser.id) {
+      res.status(400).json({msg:'new user needs id'})
+    } 
+    users.push(newUser)
+    res.json(users)
+})
+
 
 
 /* END - create routes here */
